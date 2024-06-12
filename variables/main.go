2b45a7eed5e2135b189161e23edcddaeb5157900
huseyin uzum <hsynuzm42@gmail.com>
2024-06-12 14:08:57 +0300

    variables

--- variables/main.go
+++ variables/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
 func main() {
 
 	/* first method
@@ -27,7 +22,21 @@ func main() {
 	var productName string = "i ma learning go"
 	fmt.Println(productName, reflect.TypeOf(productName)) */
 
+	/* third method
 	productName := "i am learning go"
-	fmt.Println(productName, reflect.TypeOf(productName))
+	fmt.Println(productName, reflect.TypeOf(productName))*/
+
+	var productName string
+	var quantity int
+	var discount float32
+	var isInStock bool
+
+	productName = "i am learning go"
+	quantity = 4
+	discount = 0.35
+	isInStock = true
+
+	//fmt.Println("prduct name: ", productName, "Quantity: ", quantity, "Discount: ", discount, "Is in stock? :", isInStock)
+	//fmt.Printf("Product name: %s, Quantity: %d, Discount: %.2f, Is in stock? : %t\n", productName, quantity, discount, isInStock) --> Printf
 
 }

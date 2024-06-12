467547ecdfc85d2deb34941c8077046e9bd705eb
huseyin uzum <hsynuzm42@gmail.com>
2024-06-12 14:32:59 +0300

    arrays and Slices

--- arrays_slices/main.go
+++ arrays_slices/main.go
@@ -5,11 +5,16 @@ import "fmt"
 func main() {
 
 	//Fixed
+	/*first method
 	var names [3]string
 
 	names[0] = "jhon"
 	names[1] = "james"
 	names[2] = "cena"
 
+	fmt.Println(names)*/
+
+	var names = [3]string{"jhon", "james", "cena"}
+	names[1] = "jane"
 	fmt.Println(names)
 }

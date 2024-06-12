a1461d27819a38d98371f58ebd04d275ce5b01e8
huseyin uzum <hsynuzm42@gmail.com>
2024-06-12 14:22:03 +0300

    Variables

---
+++ arrays_slices/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Fixed
+	var names [3]string
+
+	names[0] = "jhon"
+	names[1] = "james"
+	names[2] = "cena"
+
+	fmt.Println(names)
+}
--- variables/main.go
+++ variables/main.go
@@ -26,17 +26,18 @@ func main() {
 	productName := "i am learning go"
 	fmt.Println(productName, reflect.TypeOf(productName))*/
 
-	var productName string
-	var quantity int
-	var discount float32
-	var isInStock bool
+	/*
+		var productName string
+		var quantity int
+		var discount float32
+		var isInStock bool
 
-	productName = "i am learning go"
-	quantity = 4
-	discount = 0.35
-	isInStock = true
+		productName = "i am learning go"
+		quantity = 4
+		discount = 0.35
+		isInStock = true
 
-	//fmt.Println("prduct name: ", productName, "Quantity: ", quantity, "Discount: ", discount, "Is in stock? :", isInStock)
-	//fmt.Printf("Product name: %s, Quantity: %d, Discount: %.2f, Is in stock? : %t\n", productName, quantity, discount, isInStock) --> Printf
+		fmt.Println("prduct name: ", productName, "Quantity: ", quantity, "Discount: ", discount, "Is in stock? :", isInStock)
+		fmt.Printf("Product name: %s, Quantity: %d, Discount: %.2f, Is in stock? : %t\n", productName, quantity, discount, isInStock) --> Printf*/
 
 }
